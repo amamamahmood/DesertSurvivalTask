8c6f1f948184c8319abb9c744ed686fcdd850759
amamamahmood <amamamahmood@users.noreply.github.com>
2021-07-30 08:15:38 -0400

    publish

--- DesertSurvivorTask/deploy.sh
+++ DesertSurvivorTask/deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:username/DesertSurvivalTask/DesertSurvivorTask.git master:gh-pages
+git push -f git@github.com:username/DesertSurvivalTask.git master:gh-pages
 cd -
