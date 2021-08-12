db923bffb4b27cfdd4ac531d62c9bded0544ea11
amamamahmood <amahmo11@jhu.edu>
2021-08-11 20:10:47 -0400

    Update store.js

--- src/components/store.js
+++ src/components/store.js
@@ -6,16 +6,23 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        gender: 0
+        gender: 0,
+        u_id: null
     },
     getters: {
         getGender: state => {
             return state.gender;
+        },
+        getUid: state => {
+            return state.u_id;
         }
     },
     mutations: {
         storeGender(state, genderA) {
             state.gender = genderA;
+        },
+        storeUid(state, uid) {
+            state.u_id = uid;
         }
     }
 });
