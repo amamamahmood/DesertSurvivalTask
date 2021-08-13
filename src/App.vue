27ffb21056eccb3260536424f3320398403414e3
amamamahmood <amahmo11@jhu.edu>
2021-08-13 09:15:30 -0400

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
     <div id="app" class="unselectable">
-        <div id="surveyElement" style="object-fit:scale-down;width:100vw; padding-left:0">
-            <PostSurvey />
-        </div>
         <div class="row">
 
 
@@ -12,7 +9,7 @@
                     <li class="float-child" style="list-style-position: inside; font-size:1.8vmin;" v-for="item in avatarList" :key="item.id">
                         <div>
                             <img :src="item.avatar" alt="item.name" width="70" heigth="70" style="width: 6vw; height: 6vw; display: block; margin: auto;">
-                            <p style="display: block; font-size: 1.8vmin;">{{item.name}}</p>
+                            <p  style="display: block; font-size: 1.8vmin;">{{item.name}}</p>
                         </div>
                     </li>
                 </ol>
@@ -28,7 +25,9 @@
 
 
             </div>
-
+            <div id="surveyElement" class="columnSurvey">
+                <SurveyComponent />
+            </div>
             <div id="surveyElement2" style="display:none;" class="columnSurvey2">
                 <PostSurvey id="postSurvey" />
             </div>
@@ -80,9 +79,8 @@
 
 
         </div>
-
     </div>
-    
+
 </template>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
